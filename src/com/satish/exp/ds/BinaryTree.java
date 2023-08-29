37f5f399af2950fa2fad706a61fd3751d86183d9
satishetc4288 <satishetc4288@gmail.com>
2023-08-29 18:51:39 +0530

    added binary tree class

--- src/com/satish/exp/ds/BinaryTree.java
+++ src/com/satish/exp/ds/BinaryTree.java
@@ -13,23 +13,33 @@ public class BinaryTree {
          }
      }
 
-    private Node root;
+     private Node root;
+     private static int height;
+     private static int findNodeDepth(Node node, int data){
 
-    private static int findNodeDepth(Node node, int data){
-
-        if(node == null){
-            return -1;
-        }
+        if(node == null) return -1;
 
         int dist = -1;
-
-        if(node.data == data ||
-                (dist = findNodeDepth(node.left, data)) >= 0 ||
+        if(node.data == data || (dist = findNodeDepth(node.left, data)) >= 0 ||
                 (dist = findNodeDepth(node.right, data)) >= 0)
             return (dist+1);
 
         return dist;
 
+     }
+
+    private static int findHeightUtil(Node root, int x)
+    {
+        if (root == null) return -1;
+
+        int leftHeight = findHeightUtil(root.left, x);
+        int rightHeight = findHeightUtil(root.right, x);
+
+        int ans = Math.max(leftHeight, rightHeight) + 1;
+
+        if (root.data == x) System.out.printf("hieght is: " + ans);
+
+        return ans;
     }
 
     public static void main(String[] args){
@@ -44,9 +54,7 @@ public class BinaryTree {
 
         int k = 15;
 
-        // Function call to find the
-        // depth of a given node
         System.out.println("Depth: " + findNodeDepth(root, k));
-
+        findHeightUtil(root, 10);
     }
 }
