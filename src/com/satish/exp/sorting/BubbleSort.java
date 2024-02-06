a08f2e2ac191d6b93154b521d00e914802dde308
satish rajput <satishetc4288@gmail.com>
2024-02-06 17:34:54 +0530

    added few more sortings

--- src/com/satish/exp/sorting/BubbleSort.java
+++ src/com/satish/exp/sorting/BubbleSort.java
@@ -3,28 +3,34 @@ package com.satish.exp.sorting;
 import java.util.Arrays;
 
 public class BubbleSort {
+
     public static void main(String[] args) {
-        long[] arr = {1,3,2,3,4,2,6,5,7,6,5,6,3};
-        for (int i=0; i<arr.length; i++){
-            System.out.println(arr[i]);
-        }
-        System.out.println("----##__");
-        bubbleSort(arr);
-        for (int i=0; i<arr.length; i++){
-            System.out.println(arr[i]);
-        }
-    }
-
-    private static void bubbleSort(long[] arr){
-
-        for (int i=0; i<arr.length; i++){
-            for (int j=0; j<i; j++){
-                if( arr[i] < arr[j] ){
-                    long temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
-                }
+        String input = "a@bcd#EF";
+        char[] arr = input.toCharArray();
+        //Output : "F@Edc#ba"
+        int i = 0;
+        int j = input.length()-1;
+        while(i<j){
+            boolean a = String.valueOf(arr[i]).matches("[a-zA-Z]");
+            boolean b = String.valueOf(arr[j]).matches("[a-zA-Z]");
+            if(!a){
+                i++;
+            }
+            if(!b){
+                j--;
+            }
+            if(a && b ){
+                char temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+                j--;
             }
         }
+
+        for(char c: arr){
+            System.out.println("letter: " + c);
+        }
+
     }
 }
